fb84e75dd8ff50dad587bef106fb729a953d8629
Karthik Kumaresan <killerrookthegreat@gmail.com>
2023-12-23 15:06:07 +0530

    second commit

---
+++ ifStatements/comStrictEqual.js
@@ -0,0 +1,18 @@
+// Setup
+function testStrict(val) {
+  if (val === 7) {
+    // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+console.log(testStrict("7"));
+
+// output should be False
+
+/*
+3 === 3
+3 === '3'
+*/
